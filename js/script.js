96d887184a7e861b0e6024578568bad23924b403
Jonathan East <jeast3113@gmail.com>
2022-08-04 07:17:06 -0400

    created function and variable for getComputerChoice; console.log-ged the choices to ensure variable and function were set up correctly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+
+
+// Create a function called getComputerChoice that randomly selects 'rock'
+// 'paper' or scissors and returns the result
+
+function getComputerChoice() {
+    // need a variable to hold the choices - create an array to hold the choices
+    let choices = ['rock', 'paper', 'scissors'];
+    console.log(choices);
+}
+
+// call the function to console.log the variable
+getComputerChoice();
