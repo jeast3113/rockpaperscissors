7855889cec2d929c22d4320fb622459a120c2e2e
Jonathan East <jeast3113@gmail.com>
2022-08-04 07:22:14 -0400

    working on getComputerChoice to produce random choice; current getting random number between 0 and 1; revisiting how to get selection from array

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@
 function getComputerChoice() {
     // need a variable to hold the choices - create an array to hold the choices
     let choices = ['rock', 'paper', 'scissors'];
-    console.log(choices);
+    // console.log(choices);
+    // create a randomizer algorithm with the choice variable - create 
+    // another variable to hold the random choice - i.e. let randomChoice = 
+    // revisit the Math.random method...
+    let randomChoice = Math.random() * choices.length;
+    console.log(randomChoice);
+
 }
 
 // call the function to console.log the variable
