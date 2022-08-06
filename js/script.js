a33e622b7e9aa758f95f3ab4b7e174ade065476d
Jonathan East <jeast3113@gmail.com>
2022-08-06 15:15:54 -0400

    created playRound function and added conditionals; will test function with a round through console

--- js/script.js
+++ js/script.js
@@ -27,3 +27,22 @@ getComputerChoice();
 // 2. The parameters should be the player's choice and the computer's choice
 // 3. Return a string that determines the outcome of the play; i.e. "You lose! Paper beats Rock!"
 //    Determining the outcome of a round suggests an if - else if statement for all outcomes
+function playRound(playerSelection, computerSelection) {
+    if((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || playerSelection === 'scissors' && computerSelection === 'scissors' ) {
+        return `It's a tie!`;
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return `You win! Rock beats Scissors!`;
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose! Rock beats Scissors!`;
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return `You win! Paper beats Rock!`;
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return `You lose! Paper beats Rock!`;
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You win! Scissors beats Paper!`;
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        return `You lose! Scissors beats Paper!`;
+    }
+}
+
+//test out the 'playRound' function 
