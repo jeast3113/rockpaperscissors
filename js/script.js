18a9e08b3ba56b90da34b1d536ac94d920f7d29c
Jonathan East <jeast3113@gmail.com>
2022-08-15 20:07:49 -0400

    moved variables to global; removed comments from js file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
 //global variables
 let playerScore = 0;
 let cpuScore = 0;
+let playerSelection;
+let computerSelection = getComputerChoice();
 
-// Create a function called getComputerChoice that randomly selects 'rock'
-// 'paper' or scissors and returns the result
 
 function getComputerChoice() {
-    // need a variable to hold the choices - create an array to hold the choices
     let choices = ['rock', 'paper', 'scissors'];
-    // console.log(choices);
-    // create a randomizer algorithm with the choice variable - create 
-    // another variable to hold the random choice - i.e. let randomChoice = 
-    // revisit the Math.random method...
-    // problem solving moment - How do you get a choice from the choices array?
-    // problem solving moment - How do you get a rounded number to act as an index of the choices array?
-    // problem solving moment - Anything multipled by zero returns zero...
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 
 }
 
-// Create a function that plays a single round of rock, paper, scissors.
-// Steps to take:
-// 1. Function has 2 parameters i.e. function func(param1, param2)
-// 2. The parameters should be the player's choice and the computer's choice
-// 3. Return a string that determines the outcome of the play; i.e. "You lose! Paper beats Rock!"
-//    Determining the outcome of a round suggests an if - else if statement for all outcomes
 function playRound(playerSelection, computerSelection) {  
     if((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || playerSelection === 'scissors' && computerSelection === 'scissors' ) {
         return "It's a tie! Current score: " + playerScore + " to " + cpuScore;
@@ -53,8 +39,7 @@ function playRound(playerSelection, computerSelection) {
 // Create a function that plays 5 rounds, keeps score between the player and the computer
 // and determines a winner at the end of the game
 function game() {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
+
     // if(playerScore > cpuScore) {
     //     return `Player Wins ${playerScore} to ${cpuScore}!`;
     // } else if(playerScore < cpuScore){
@@ -65,7 +50,3 @@ function game() {
 }
 
 console.log(game());
-const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', playRound);
-    });
