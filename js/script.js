bab0005303ff36ab980113a666ed33472720ebf0
Jonathan East <jeast3113@gmail.com>
2022-08-16 16:29:07 -0400

    updated js file with global variables creating elements and appending to divs

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ let playerSelection;
 let buttons = document.querySelectorAll('.btn');
 let btnContainerDiv = document.querySelector('.button-container');
 let resultsContainerDiv = document.querySelector('.results-container')
-let p = document.createElement('p');
+let scorePara = document.createElement('p');
+let finalResultPara = document.createElement('p');
 
 
 
@@ -18,32 +19,32 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {  
     if((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || playerSelection === 'scissors' && computerSelection === 'scissors' ) {
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore+=1;
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
         cpuScore+=1;
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     } else if(playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore+=1;
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     } else if(playerSelection === 'rock' && computerSelection === 'paper') {
         cpuScore+=1;
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore+=1;
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
         cpuScore+=1;
-        p.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
-        resultsContainerDiv.appendChild(p);
+        scorePara.textContent = `Player: ${playerScore} | Computer: ${cpuScore}`
+        resultsContainerDiv.appendChild(scorePara);
     }
 }
 
@@ -56,11 +57,13 @@ function game() {
             computerSelection = getComputerChoice();
             playRound(playerSelection, computerSelection);
             if(playerScore === 5) {
-                p.textContent = `Player Wins ${playerScore} to ${cpuScore}!`;
-                resultsContainerDiv.appendChild(p);
+                finalResultPara.textContent = `Player Wins ${playerScore} to ${cpuScore}!`;
+                resultsContainerDiv.removeChild(scorePara);
+                resultsContainerDiv.appendChild(finalResultPara);
             } else if(cpuScore === 5){
-                p.textContent = `Computer Wins ${cpuScore} to ${playerScore}`;
-                resultsContainerDiv.appendChild(p);
+                finalResultPara.textContent = `Computer Wins ${cpuScore} to ${playerScore}`;
+                resultsContainerDiv.removeChild(scorePara);
+                resultsContainerDiv.appendChild(finalResultPara);
             }
         });
     });
